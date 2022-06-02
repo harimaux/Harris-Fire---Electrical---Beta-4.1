156be95c22df530053e85f85e35e4c66700d0c9d
harimaux <lukaszwitek20@googlemail.com>
2022-06-02 17:00:06 +0100

    home, fire v.1.0

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Mobile Menu
+const mobileBtn = document.querySelector(".mobileMenuBtn");
+const mobileNav = document.querySelector(".mobileNav");
+
+//Hides and show mobile menu
+mobileBtn.addEventListener("click", () => {
+  mobileNav.classList.toggle("navToggle");
+
+  //Applies animation on button when clicked
+  const menuLine1 = document.getElementById("mobileBtnLine1");
+  const menuLine2 = document.getElementById("mobileBtnLine2");
+  const menuLine3 = document.getElementById("mobileBtnLine3");
+
+  menuLine1.classList.toggle("mobileBtnLine1X");
+  menuLine2.classList.toggle("mobileBtnLine2X");
+  menuLine3.classList.toggle("mobileBtnLine3X");
+
+  menuLine1.classList.toggle("mobileBtnColor");
+  menuLine2.classList.toggle("mobileBtnColor");
+  menuLine3.classList.toggle("mobileBtnColor");
+});
